ff8c08f4c9664b9c6537392d447d45c73c25d838
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-10-21 00:10:13 +0300

    add mongoApi

--- components/sections/Banner/BannerSection.tsx
+++ components/sections/Banner/BannerSection.tsx
@@ -1,91 +1,46 @@
 import {BannerSwiper} from "@/components/elements/swiper/swiper";
-import clientPromise from "@/services/mongoDB";
-
-export interface IDocument {
-  _id: IId;
-  title: string;
-  imgLink: string;
-}
-
-export interface IId {
-  $oid: string;
-}
+import {mongoApi} from "@/services/mongoApi";
 
 const {MONGODB_API_KEY} = process.env;
 
 export const BannerSection = async () => {
-  // const res = await fetch("http://localhost:3000/api/slides");
-  // Novik
-  // const res = await fetch(`/api/slides`);
-  /////////////////////
-  // const res = await fetch(`${process.env.BASE_HOST}/api/slides`);
-  // const data = await res.json();
-  // const slides = data?.data;
-  /////////////////////////
+  // const res = await fetch(
+  //   "https://eu-central-1.aws.data.mongodb-api.com/app/data-alsgc/endpoint/data/v1/action/find",
+  //   {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Access-Control-Request-Headers": "*",
+  //       "api-key": MONGODB_API_KEY!,
 
-  // const client = await clientPromise;
-  // const db = client.db("db-site-content");
+  //       Accept: "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       collection: "slides",
+  //       database: "db-site-content",
+  //       dataSource: "Cluster-Nadia-Mentii",
+  //     }),
+  //   }
+  // );
 
-  // const data = await db.collection("slides").find().toArray();
+  const mongoApiParams = {
+    action: "find",
+    data: null,
+    collection: "slides",
+  };
 
-  // const slides = data.map((el) => ({
-  //   _id: el._id.toString(),
-  //   title: el.title.toString(),
-  //   imgLink: el.imgLink.toString(),
-  // }));
+  const res = await mongoApi(mongoApiParams);
 
-  //   curl --location --request POST 'https://eu-central-1.aws.data.mongodb-api.com/app/data-alsgc/endpoint/data/v1/action/findOne' \
-  // --header 'Content-Type: application/json' \
-  // --header 'Access-Control-Request-Headers: *' \
-  // --header 'api-key: hmYaEGFYYKUAzKz8IH6xKjPDJXovAYkxbFyFWVUnQ2QKZdKbjH2cxcXQufVyMZdl' \
-  // --header 'Accept: application/ejson' \
-  // --data-raw '{
-  //     "collection":"<COLLECTION_NAME>",
-  //     "database":"db-site-content",
-  //     "dataSource":"Cluster-Nadia-Mentii",
-  //     "projection": {"_id": 1}
-  // }'
+  const initialSlides = [{_id: "1", title: "", imgLink: ""}];
 
-  // //
-
-  const res = await fetch(
-    // "https://eu-central-1.aws.data.mongodb-api.com/app/data-alsgc/endpoint/data/v1/action/findOne",
-    "https://eu-central-1.aws.data.mongodb-api.com/app/data-alsgc/endpoint/data/v1/action/find",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Request-Headers": "*",
-        "api-key": MONGODB_API_KEY!,
-        // "api-key":
-        //   "hmYaEGFYYKUAzKz8IH6xKjPDJXovAYkxbFyFWVUnQ2QKZdKbjH2cxcXQufVyMZdl",
-        Accept: "application/ejson",
-      },
-      body: JSON.stringify({
-        collection: "slides",
-        database: "db-site-content",
-        dataSource: "Cluster-Nadia-Mentii",
-        // projection: {_id: 1},
-      }),
-    }
-  );
-
-  const {documents} = await res.json();
-  console.log("documents", documents);
-
-  const slides = documents.map((el: IDocument) => ({
-    _id: el._id["$oid"].toString(),
-    title: el.title.toString(),
-    imgLink: el.imgLink.toString(),
-  }));
+  const slides = res?.documents ? res?.documents : initialSlides;
 
   return (
     <section
       style={{
         paddingTop: 50,
         paddingBottom: 100,
-        // marginTop: 50,
-        // marginBottom: 50,
+
         backgroundColor: "rgba(51, 15, 179, 0.1)",
       }}
     >
---
+++ services/mongoApi.ts
@@ -0,0 +1,43 @@
+type MongoApiParams = {
+  action: string;
+  data: object | null;
+  collection: string;
+};
+
+type MongoApiBody = {
+  collection: string;
+  database: string;
+  dataSource: string;
+  projection?: object;
+};
+
+const {MONGODB_API_KEY} = process.env;
+
+export const mongoApi = async ({action, data, collection}: MongoApiParams) => {
+  const body: MongoApiBody = {
+    collection: collection,
+    database: "db-site-content",
+    dataSource: "Cluster-Nadia-Mentii",
+  };
+
+  if (data) {
+    body.projection = data;
+  }
+
+  const res = await fetch(
+    `https://eu-central-1.aws.data.mongodb-api.com/app/data-alsgc/endpoint/data/v1/action/${action}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Request-Headers": "*",
+        "api-key": MONGODB_API_KEY!,
+
+        Accept: "application/json",
+      },
+      body: JSON.stringify(body),
+    }
+  );
+
+  return await res.json();
+};
