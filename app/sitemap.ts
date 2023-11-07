bb77c280687f47c03420b3b7d63f85d0161b68e3
unknown <a.novychenko@outlook.com>
2023-11-07 12:31:15 +0200

    fix url sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,30 +1,30 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'hhttps://nm-massagekiev.vercel.app',
+      url: "https://nm-massagekiev.vercel.app",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: 'https://nm-massagekiev.vercel.app/news',
+      url: "https://nm-massagekiev.vercel.app/news",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.3,
     },
     {
-      url: 'https://nm-massagekiev.vercel.app/questions',
+      url: "https://nm-massagekiev.vercel.app/questions",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.5,
     },
     {
-      url: 'https://nm-massagekiev.vercel.app/about',
+      url: "https://nm-massagekiev.vercel.app/about",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.8,
     },
-  ]
+  ];
 }
