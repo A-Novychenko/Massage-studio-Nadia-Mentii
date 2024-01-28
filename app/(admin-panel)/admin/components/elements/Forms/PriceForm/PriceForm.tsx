4e86337c9b6348f73b1bfd940006354a10aa33c4
A-Novychenko <A.Novychenko@outlook.com>
2024-01-28 20:45:59 +0200

    test

--- app/(admin-panel)/admin/components/elements/Forms/PriceForm/PriceForm.tsx
+++ app/(admin-panel)/admin/components/elements/Forms/PriceForm/PriceForm.tsx
@@ -49,6 +49,8 @@ export const AdminPriceForm = () => {
           {errors.service && <p role="alert">{errors.service.message}</p>}
         </div>
 
+        {/* test push to github */}
+
         <div className={styles.input_container}>
           <label className={styles.input_label}>
             <span className={styles.requared}>*</span>Тривалість
